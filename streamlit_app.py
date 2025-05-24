a7c22d4822015122b615e3db17e03e5d239f92cd
kxkuah <kxkuah@gmail.com>
2025-05-24 19:54:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ df, parameter_names = load_data()
 
 target_alloy = st.selectbox("Select Target Alloy", df.index)
 fixed_alloys = st.multiselect("Select Fixed Alloys", [a for a in df.index if a != target_alloy])
-n_additional = st.number_input("Number of Additional Contributing Alloys", min_value=1, max_value=10, value=2, step=1)
+n_additional = st.number_input("Number of Additional Contributing Alloys", min_value=0, max_value=10, value=2, step=1)
 
 # Default ranges for elements
 default_ranges = {
